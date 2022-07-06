58a7e343c574046ed99d895ecd7d9063e9bb16b2
bn-tw2020 <skaxodn97@gmail.com>
2022-07-06 11:48:25 +0900

    feat: drawer navigationView 설정
    - drawer Header 레이아웃 설정
    - drawer menu 설정
    - 케이스에 따른 drawer close

--- app/src/main/java/com/woowahan/mail/home/HomeActivity.kt
+++ app/src/main/java/com/woowahan/mail/home/HomeActivity.kt
@@ -2,6 +2,9 @@ package com.woowahan.mail.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import com.woowahan.mail.R
 import com.woowahan.mail.databinding.ActivityHomeBinding
@@ -13,5 +16,43 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+
+        initView()
+    }
+
+    private fun initView() {
+        binding.tbMail.setNavigationOnClickListener { toggleDrawer() }
+        binding.nvView.itemIconTintList = null
+        binding.nvView.setNavigationItemSelectedListener { setDrawerListener(it) }
+    }
+
+    private fun setDrawerListener(it: MenuItem): Boolean {
+        when (it.itemId) {
+            R.id.primary -> {
+                Toast.makeText(this@HomeActivity, "primary", Toast.LENGTH_SHORT).show()
+            }
+            R.id.social -> {
+                Toast.makeText(this@HomeActivity, "social", Toast.LENGTH_SHORT).show()
+            }
+            R.id.promotions -> {
+                Toast.makeText(this@HomeActivity, "promotions", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.drawerLayout.closeDrawer(binding.nvView)
+        return false
+    }
+
+    private fun toggleDrawer() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START))
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        else
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START))
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        else
+            super.onBackPressed()
     }
 }
