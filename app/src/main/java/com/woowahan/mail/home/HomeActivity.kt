3f32b31dba50d12fc4a5aa18be761a7d50c17dad
bn-tw2020 <skaxodn97@gmail.com>
2022-07-06 16:54:57 +0900

    feat: fragment 생성
    - primary, social. promotion 프래그먼트 생성

--- app/src/main/java/com/woowahan/mail/home/HomeActivity.kt
+++ app/src/main/java/com/woowahan/mail/home/HomeActivity.kt
@@ -1,13 +1,18 @@
 package com.woowahan.mail.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentManager
 import com.woowahan.mail.R
 import com.woowahan.mail.databinding.ActivityHomeBinding
+import com.woowahan.mail.home.mail.MailFragment
+import com.woowahan.mail.home.mail.primary.PrimaryFragment
+import com.woowahan.mail.home.mail.promotion.PromotionFragment
+import com.woowahan.mail.home.mail.social.SocialFragment
+import com.woowahan.mail.home.settings.SettingsFragment
 
 class HomeActivity : AppCompatActivity() {
 
@@ -24,20 +29,50 @@ class HomeActivity : AppCompatActivity() {
         binding.tbMail.setNavigationOnClickListener { toggleDrawer() }
         binding.nvView.itemIconTintList = null
         binding.nvView.setNavigationItemSelectedListener { setDrawerListener(it) }
+        binding.bnMain.setOnItemSelectedListener { setBottomNavigationListener(it) }
+    }
+
+    private fun setBottomNavigationListener(it: MenuItem): Boolean {
+        val transaction = supportFragmentManager.beginTransaction()
+        when (it.itemId) {
+            R.id.navigation_mail -> {
+                val mailFragment = MailFragment()
+                transaction.replace(R.id.container_view, mailFragment, "mail")
+                transaction.addToBackStack("mail")
+            }
+            R.id.navigation_settings -> {
+                val settingsFragment = SettingsFragment()
+                transaction.replace(R.id.container_view, settingsFragment, "settings")
+                transaction.addToBackStack("settings")
+            }
+        }
+        transaction.commit()
+        return true
     }
 
     private fun setDrawerListener(it: MenuItem): Boolean {
+        val transaction = supportFragmentManager.beginTransaction()
         when (it.itemId) {
             R.id.primary -> {
-                Toast.makeText(this@HomeActivity, "primary", Toast.LENGTH_SHORT).show()
+                binding.bnMain.menu.findItem(R.id.navigation_mail).isChecked = true
+                val primaryFragment = PrimaryFragment()
+                transaction.replace(R.id.container_view, primaryFragment, "primary")
+                transaction.addToBackStack("primary")
             }
             R.id.social -> {
-                Toast.makeText(this@HomeActivity, "social", Toast.LENGTH_SHORT).show()
+                binding.bnMain.menu.findItem(R.id.navigation_mail).isChecked = true
+                val socialFragment = SocialFragment()
+                transaction.replace(R.id.container_view, socialFragment, "social")
+                transaction.addToBackStack("social")
             }
             R.id.promotions -> {
-                Toast.makeText(this@HomeActivity, "promotions", Toast.LENGTH_SHORT).show()
+                binding.bnMain.menu.findItem(R.id.navigation_mail).isChecked = true
+                val promotionFragment = PromotionFragment()
+                transaction.replace(R.id.container_view, promotionFragment, "promotions")
+                transaction.addToBackStack("promotions")
             }
         }
+        transaction.commit()
         binding.drawerLayout.closeDrawer(binding.nvView)
         return false
     }
@@ -49,10 +84,29 @@ class HomeActivity : AppCompatActivity() {
             binding.drawerLayout.openDrawer(GravityCompat.START)
     }
 
+    private fun updateBottomMenu() {
+        val mailTag = supportFragmentManager.findFragmentByTag("mail")
+        val settingsTag = supportFragmentManager.findFragmentByTag("settings")
+        val primaryTag = supportFragmentManager.findFragmentByTag("primary")
+        val socialTag = supportFragmentManager.findFragmentByTag("social")
+        val promotionsTag = supportFragmentManager.findFragmentByTag("promotions")
+
+        if (mailTag != null && mailTag.isVisible) {
+            binding.bnMain.menu.findItem(R.id.navigation_mail).isChecked = true
+        } else if (settingsTag != null && settingsTag.isVisible) {
+            binding.bnMain.menu.findItem(R.id.navigation_settings).isChecked = true
+        }
+        if (primaryTag != null || socialTag != null || promotionsTag != null) {
+            supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        }
+    }
+
     override fun onBackPressed() {
-        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START))
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
             binding.drawerLayout.closeDrawer(GravityCompat.START)
-        else
+        } else {
             super.onBackPressed()
+            updateBottomMenu()
+        }
     }
 }
