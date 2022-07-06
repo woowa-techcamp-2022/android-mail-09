48d5d9fb1be2e5e13675bbdfbea52a1bedf7e024
bn-tw2020 <skaxodn97@gmail.com>
2022-07-06 10:32:16 +0900

    feat: 입력정보화면 레이아웃
    - 데이터바인딩 설정
    - material design textField editText

--- app/src/main/java/com/woowahan/mail/MainActivity.kt
+++ app/src/main/java/com/woowahan/mail/MainActivity.kt
@@ -2,10 +2,15 @@ package com.woowahan.mail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.woowahan.mail.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
