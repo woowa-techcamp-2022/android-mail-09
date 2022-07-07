49b9d78301a4750278bbdec9c63831394e3723e5
bn-tw2020 <skaxodn97@gmail.com>
2022-07-07 22:33:59 +0900

    feat: 이메일 입력
    - 이메일 유효성 검사
      - Patterns.EMAIL_ADDRESS 이용
    - LiveData → Flow

--- app/src/main/java/com/woowahan/mail/presentation/login/MainActivity.kt
+++ app/src/main/java/com/woowahan/mail/presentation/login/MainActivity.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.doAfterTextChanged
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.woowahan.mail.R
 import com.woowahan.mail.databinding.ActivityMainBinding
 import com.woowahan.mail.presentation.home.HomeActivity
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,9 +30,6 @@ class MainActivity : AppCompatActivity() {
     private fun initView() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
-        binding.etLayoutNickname.editText?.doAfterTextChanged {
-            viewModel.validate(it.toString())
-        }
         binding.btnNext.setOnClickListener {
             val intent = Intent(this@MainActivity, HomeActivity::class.java)
             startActivity(intent)
@@ -37,26 +38,72 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun observeData() {
-        viewModel.nickname.observe(this) { nickname ->
-            viewModel.validate(nickname)
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.nickname.collectLatest {
+                    viewModel.validateNickname(it)
+                }
+            }
         }
 
-        viewModel.isCorrect.observe(this) { isCorrect ->
-            if (isCorrect)
-                validNickname()
-            else
-                invalidNickname()
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.email.collectLatest {
+                    viewModel.validateEmail(it)
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.isNicknameCorrect.collectLatest {
+                    when (it) {
+                        false -> invalidNickname()
+                        true -> validNickname()
+                        else -> binding.etLayoutNickname.isErrorEnabled = false
+                    }
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.isEmailCorrect.collectLatest {
+                    when (it) {
+                        false -> invalidEmail()
+                        true -> validEmail()
+                        else -> binding.etLayoutEmail.isErrorEnabled = false
+                    }
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.isCorrect.collectLatest {
+                    binding.btnNext.isEnabled = it
+                }
+            }
         }
     }
 
     private fun validNickname() = with(binding) {
         etLayoutNickname.isErrorEnabled = false
-        btnNext.isEnabled = true
+    }
+
+    private fun validEmail() = with(binding) {
+        etLayoutEmail.isErrorEnabled = false
     }
 
     private fun invalidNickname() = with(binding) {
         etLayoutNickname.error = getString(R.string.nickname_error_message)
         etLayoutNickname.isErrorEnabled = true
+        btnNext.isEnabled = false
+    }
+
+    private fun invalidEmail() = with(binding) {
+        etLayoutEmail.error = getString(R.string.email_error_message)
+        etLayoutEmail.isErrorEnabled = true
         btnNext.isEnabled = false
     }
 
