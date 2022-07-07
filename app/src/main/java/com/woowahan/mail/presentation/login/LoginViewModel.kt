71f4196ed80866b3fa054ee75301eef19202c4d7
bn-tw2020 <skaxodn97@gmail.com>
2022-07-07 22:33:59 +0900

    feat: 이메일 입력
    - 이메일 유효성 검사
      - Patterns.EMAIL_ADDRESS 이용
    - LiveData → Flow

--- app/src/main/java/com/woowahan/mail/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/woowahan/mail/presentation/login/LoginViewModel.kt
@@ -1,19 +1,43 @@
 package com.woowahan.mail.presentation.login
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import android.util.Patterns
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.onStart
 import java.util.regex.Pattern
 
 class LoginViewModel : ViewModel() {
 
-    val nickname = MutableLiveData<String>()
+    val nickname = MutableStateFlow("")
 
-    private val _isCorrect = MutableLiveData<Boolean>()
-    val isCorrect: LiveData<Boolean> = _isCorrect
+    val email = MutableStateFlow("")
 
-    fun validate(nickname: String) {
-        val pattern = """^(?=.*[a-zA-Z])(?=.*[0-9])[0-9a-zA-Z]{4,12}$"""
-        _isCorrect.value = Pattern.matches(pattern, nickname)
+    private val _isNicknameCorrect = MutableStateFlow<Boolean?>(null)
+    val isNicknameCorrect: StateFlow<Boolean?> = _isNicknameCorrect
+
+    private val _isEmailCorrect = MutableStateFlow<Boolean?>(null)
+    val isEmailCorrect: StateFlow<Boolean?> = _isEmailCorrect
+
+    val isCorrect = combine(isNicknameCorrect, isEmailCorrect) { nickname, email ->
+        nickname == true && email == true
+    }.onStart { emit(false) }
+
+    fun validateNickname(nickname: String) {
+        if (nickname.isEmpty()) {
+            _isNicknameCorrect.value = null
+            return
+        }
+        val pattern = "^(?=.*[a-zA-Z])(?=.*[0-9])[0-9a-zA-Z]{4,12}$"
+        _isNicknameCorrect.value = Pattern.matches(pattern, nickname)
+    }
+
+    fun validateEmail(email: String) {
+        if (email.isEmpty()) {
+            _isEmailCorrect.value = null
+            return
+        }
+        _isEmailCorrect.value = Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 }
